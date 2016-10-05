0f976c5df854e78a3ad7d5b1cc7614997662f673
r-vijay-r <503r.vijay@gmail.com>
2016-10-05 16:47:27 +0530

    working on login-out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import * as firebase from 'firebase';
+import { AuthProviders,AuthMethods} from 'angularfire2';
+
+const firebaseConfig = {
+ apiKey: "AIzaSyBuhNGCwz29zR5KzVMuCzV1U0xxKSjfO9E",
+ authDomain: "test-b4797.firebaseapp.com",
+ databaseURL: "https://test-b4797.firebaseio.com/",
+ storageBucket: "test-b4797.appspot.com"
+}
+const AuthenticationConfig = {
+   method: AuthMethods.Popup,
+   provider: AuthProviders.Google
+ }
 
 @NgModule({
   declarations: [
@@ -12,7 +26,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, AuthenticationConfig)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
